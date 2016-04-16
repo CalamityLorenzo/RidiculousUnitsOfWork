6f79c32e035cd754fe7aee3a48c2c75900c95c8a
Calamity Lorenzo <lawrence_paul@hotmail.co.uk>
2016-04-16 10:50:08 +0100

    Bing abinka bong

--- Geek.Blog.Posts.v2/Services/BlogManager.cs
+++ Geek.Blog.Posts.v2/Services/BlogManager.cs
@@ -9,16 +9,28 @@ namespace Geek.Blog.Posts.Services
 {
     // Handles the connections and stuff
     // instantiate this, you create the entire application
-    public class BlogManager
+    public class BlogManager : IDisposable
     {
+
+
         public IPostService PostService { get; }
         public IPostsInfoService PostInfo { get; }
         public ITagsService PostTags { get; }
+        private IBlogUnitOfWork bunitOfWork;
         public BlogManager(IBlogUnitOfWork blogData)
         {
             PostService = new PostService(blogData.Posts);
             PostInfo = new PostsInfoService(blogData.PostInfo);
             PostTags = new TagsService(blogData.Tags);
+            this.bunitOfWork = blogData;
+        }
+
+        public void Dispose()
+        {
+           if(bunitOfWork != null)
+            {
+                this.bunitOfWork.Dispose();
+            }
         }
     }
 }
